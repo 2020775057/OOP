5bda16a2ff480706e14a6c4c73f8facc6646e150
ur <dorosi150@naver.com>
2022-03-15 22:25:37 +0900

    0315

---
+++ Year/Year/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("년도를 입력하세요: ");
+            string st = Console.ReadLine();
+            int year = int.Parse(st);
+            if (year%4 == 0)
+            {
+                if ((year % 400 == 0) || year % 100 != 0) Console.WriteLine("윤년입니다.");
+                else Console.WriteLine("평년입니다.");
+            }
+            else Console.WriteLine("평년입니다.");
+
+        }
+    }
+}
