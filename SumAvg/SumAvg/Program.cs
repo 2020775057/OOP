cb3154e298a7b129b9da2bc28992f3f1c1d0d2d5
ur <dorosi150@naver.com>
2022-03-29 21:11:24 +0900

    0329

---
+++ SumAvg/SumAvg/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumAvg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            SA(arr);
+        }
+        static void SA(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++){
+                sum += arr[i];
+            }
+            Console.WriteLine("합: "+ sum);
+            Console.WriteLine("평균: "+(sum / arr.Length));
+        }
+    }
+}
