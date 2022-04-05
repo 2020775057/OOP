d4973631d598265448e47852961d4b3cdefd2fa1
ur <dorosi150@naver.com>
2022-04-05 23:17:07 +0900

    0405

---
+++ CI/CI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI
+{
+     class A
+    {
+        public void Parent() {
+            Console.WriteLine("Parent Class");
+        }
+    }
+    class B : A
+    {
+        public void Child()
+        {
+            Console.WriteLine("Child Class");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B ch = new B();
+            ch.Parent();
+            ch.Child();
+        }
+    }
+}
