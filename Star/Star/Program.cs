1f0087b9e3fd2328344c5567ccf2faaceba6e011
ur <dorosi150@naver.com>
2022-04-05 23:14:28 +0900

    0405

---
+++ Star/Star/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=6; i++)
+            {
+                for(int j=i; j<6; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j=1; j<=2*i-1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
